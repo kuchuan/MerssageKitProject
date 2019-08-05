b7f2db582ec59bef0b8c13571e908124e81cc0d1
堀川浩二 <>
2019-08-05 10:15:38 +0800

    FirstCommit

---
+++ MsssageKitProject/Objects/ChatUser.swift
@@ -0,0 +1,20 @@
+//
+//  ChatUser.swift
+//  MsssageKitProject
+//
+//  Created by 堀川浩二 on 2019/08/05.
+//  Copyright © 2019 堀川浩二. All rights reserved.
+//
+
+import MessageKit
+
+class ChatUser: SenderType {
+    
+    //ゆーざーID
+    var senderId: String
+    
+    //表示
+    var displayName: String
+    
+    <#code#>
+}
